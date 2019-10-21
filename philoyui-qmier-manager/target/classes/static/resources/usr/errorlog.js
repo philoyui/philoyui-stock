f778573c4c4943f7c56fba4643c106f04dabbf14
yangyu-ds <yangyu-ds@gome.com.cn>
2019-10-21 15:00:36 +0800

    清理target

--- philoyui-qmier-manager/target/classes/static/resources/usr/errorlog.js
+++
@@ -1,9 +0,0 @@
-function loadErrorContent(host) {
-    var aa="";
-    $.getJSON('/stat/host/error?hostname='+host, function (option) {
-        for (var  pro in option){
-            aa=option[pro];
-            $("#errortop").append(" <h5>"+aa+"</h5>")
-        }
-    });
-}
