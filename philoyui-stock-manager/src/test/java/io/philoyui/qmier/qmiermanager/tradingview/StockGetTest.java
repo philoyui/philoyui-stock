14d3a8257d03a20729276244676f803a522d26a3
yangyu-ds <yangyu-ds@gome.com.cn>
2019-11-09 19:05:54 +0800

    新浪获取数据

--- philoyui-stock-manager/src/test/java/io/philoyui/qmier/qmiermanager/tradingview/StockGetTest.java
+++ philoyui-stock-manager/src/test/java/io/philoyui/qmier/qmiermanager/tradingview/StockGetTest.java
@@ -12,19 +12,30 @@ public class StockGetTest {
     @Test
     public void test_get_stock(){
 
-        String fetchUrl = "https://pinglun.fx678.com/Ajax/changeIndexNewsByCategory";
+        String fetchUrl = "https://gu.sina.cn/ft/api/jsonp.php/var%20_SI_15_1573205959334=/GlobalService.getMink?symbol=SI&type=15";
         Connection connect = Jsoup.connect(fetchUrl);
         connect.ignoreContentType(true);
         connect.header("Content-Type", "application/json");
-        connect.data("category_id","102,103");
-        connect.data("pageSize","12");
-        connect.data("page","1");
         try {
-            Document document = connect.post();
+            Document document = connect.get();
             System.out.println(document.text());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        String fetchUrl = "https://pinglun.fx678.com/Ajax/changeIndexNewsByCategory";
+//        Connection connect = Jsoup.connect(fetchUrl);
+//        connect.ignoreContentType(true);
+//        connect.header("Content-Type", "application/json");
+//        connect.data("category_id","102,103");
+//        connect.data("pageSize","12");
+//        connect.data("page","1");
+//        try {
+//            Document document = connect.post();
+//            System.out.println(document.text());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
