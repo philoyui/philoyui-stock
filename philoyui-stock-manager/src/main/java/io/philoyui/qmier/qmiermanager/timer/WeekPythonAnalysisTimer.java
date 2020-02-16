8eb7f14a3473293c49e0aa82aa04bbfbad1f97fe
philoyui <yang7551735@gmail.com>
2020-02-16 13:23:10 +0800

    添加金融python脚本

--- philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/timer/WeekPythonAnalysisTimer.java
+++ philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/timer/WeekPythonAnalysisTimer.java
@@ -1,4 +1,32 @@
 package io.philoyui.qmier.qmiermanager.timer;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Component
 public class WeekPythonAnalysisTimer {
+
+    @Scheduled(cron="0 0/30 * * * ?") //每30分钟执行一次
+    public void fetcher(){
+        Process proc;
+        try {
+            proc = Runtime.getRuntime().exec("python3 /root/python/week_task.py");// 执行py文件
+            //用输入输出流来截取结果
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
