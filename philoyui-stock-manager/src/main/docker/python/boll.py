8f95b2f5603ecc069981a49706183794736c7343
philoyui <yang7551735@gmail.com>
2020-03-07 20:10:19 +0800

    更新脚本

---
+++ philoyui-stock-manager/src/main/docker/python/boll.py
@@ -0,0 +1,23 @@
+import talib
+
+from base import mark_stock_as_tag
+
+'''
+    周线回踩
+    参数
+        stock_data：股票信息 stock_data[5] 股票代码 stock_data[4] 股票名称
+        data_frame：股票历史数据，data_frame['close']，data_frame['low']，data_frame['high']，data_frame['open']，data_frame['volume']
+        tag_prefix：标签名字前缀（日线，周线，月线）
+'''
+
+
+def process_boll(stock_data, data_frame, tag_prefix):
+    closed = data_frame['close'].values
+    low = data_frame['low'].values
+    open = data_frame['open'].values
+
+    boll_upper, boll_middle, boll_lower = talib.BBANDS(closed, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+
+    if boll_lower[-2] < closed[-2] < open[-2] < open[-3] < boll_upper[-2] and boll_middle[-1] < closed[-1] < boll_upper[-1] and closed[-1] > \
+            open[-2] - (open[-2]-closed[-2])/2 and closed[-2] < closed[-3] and boll_middle[-1] > boll_middle[-2] and closed[-1] > open[-1]:
+        mark_stock_as_tag(stock_data, tag_prefix + "boll回踩")
