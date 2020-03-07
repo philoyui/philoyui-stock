07ae3ef1c1f8c9a2cec763fb0b7be155521344fe
philoyui <yang7551735@gmail.com>
2020-03-07 10:19:03 +0800

    更新脚本

--- philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/timer/StrategyTimer.java
+++ philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/timer/StrategyTimer.java
@@ -6,14 +6,23 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WeekStrategyTimer {
+public class StrategyTimer {
 
     @Autowired
     private StockStrategyService stockStrategyService;
 
+    @Scheduled(cron="0 0 8 1 * ?") //每月1号3点
+    public void fetcherMonth(){
+        stockStrategyService.processWithMonthTimer();
+    }
+
     @Scheduled(cron="0 30 7 ? * 7") //每周六
-    public void fetcher(){
+    public void fetcherWeek(){
         stockStrategyService.processWithWeekTimer();
     }
 
+    @Scheduled(cron="* * 20 * * ?") //下午4点
+    public void fetcher(){
+        stockStrategyService.processWithDayTimer();
+    }
 }
--- philoyui-stock-manager/src/test/java/io/philoyui/qmier/qmiermanager/SinaStockGetTest.java
+++ philoyui-stock-manager/src/test/java/io/philoyui/qmier/qmiermanager/SinaStockGetTest.java
@@ -1,9 +1,13 @@
 package io.philoyui.qmier.qmiermanager;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.philoyui.qmier.qmiermanager.entity.DataDayEntity;
+import io.philoyui.qmier.qmiermanager.entity.FinancialMarketEntity;
+import io.philoyui.qmier.qmiermanager.entity.StockEntity;
 import io.philoyui.qmier.qmiermanager.to.KLineData;
+import io.philoyui.qmier.qmiermanager.to.ProductData;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -11,6 +15,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SinaStockGetTest {
@@ -45,6 +50,74 @@ public class SinaStockGetTest {
             dataDayEntityList.add(DataDayEntity);
         }
         System.out.println(gson.toJson(dataDayEntityList));
+
+    }
+
+
+    @Test
+    public void test_stocks_get() throws IOException {
+
+        Gson gson = new GsonBuilder().create();
+
+        String fetchUrl = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=1&num=360&sort=symbol&asc=1&node=sh_a&symbol=&_s_r_a=page";
+        try {
+            Connection.Response response = Jsoup.connect(fetchUrl)
+                    .header("Content-Type", "application/json")
+                    .ignoreContentType(true)
+                    .method(Connection.Method.GET)
+                    .execute();
+            String result = response.body().replaceAll("symbol","\"symbol\"")
+                    .replaceAll("code","\"code\"")
+                    .replaceAll("name","\"name\"")
+                    .replaceAll("trade","\"trade\"")
+                    .replaceAll("pricechange","\"pricechange\"")
+                    .replaceAll("changepercent","\"changepercent\"")
+                    .replaceAll("sell","\"sell\"")
+                    .replaceAll("settlement","\"settlement\"")
+                    .replaceAll("open","\"open\"")
+                    .replaceAll("high","\"high\"")
+                    .replaceAll("low","\"low\"")
+                    .replaceAll("volume","\"volume\"")
+                    .replaceAll("amount","\"amount\"")
+                    .replaceAll("ticktime","\"ticktime\"")
+                    .replaceAll("per:","\"per\":")
+                    .replaceAll("pb","\"pb\"")
+                    .replaceAll("mktcap","\"mktcap\"")
+                    .replaceAll("nmc","\"nmc\"")
+                    .replaceAll("buy","\"buy\"")
+                    .replaceAll("turnoverratio","\"turnoverratio\"")
+                    ;
+
+            ProductData[] productDataArray = gson.fromJson(result, ProductData[].class);
+            if(productDataArray==null){
+                return;
+            }
+
+            List<StockEntity> stockEntities = Lists.newArrayList();
+
+            for (ProductData productData : productDataArray) {
+                StockEntity stockEntity = new StockEntity();
+                stockEntity.setSymbol(productData.getSymbol());
+                stockEntity.setCode(productData.getCode());
+                stockEntity.setName(productData.getName());
+                stockEntity.setModifyTime(new Date());
+                stockEntity.setPb(productData.getPb());
+                stockEntity.setPer(productData.getPer());
+                stockEntity.setTurnoverRatio(productData.getTurnOverRatio());
+                stockEntity.setTotalPrice(productData.getMktCap());
+                stockEntities.add(stockEntity);
+            }
+
+            System.out.println(stockEntities.size());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
