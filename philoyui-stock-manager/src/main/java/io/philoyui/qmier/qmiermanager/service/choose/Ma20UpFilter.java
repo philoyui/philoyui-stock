0b9a272b536b31a6081984751d0641ac1e742c47
philoyui <yang7551735@gmail.com>
2020-03-29 14:16:46 +0800

    修正定时器

--- philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/service/choose/Ma20UpFilter.java
+++ philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/service/choose/Ma20UpFilter.java
@@ -1,7 +1,6 @@
 package io.philoyui.qmier.qmiermanager.service.choose;
 
 import cn.com.gome.cloud.openplatform.common.SearchFilter;
-import com.tictactec.ta.lib.Core;
 import io.philoyui.qmier.qmiermanager.entity.StockEntity;
 import io.philoyui.qmier.qmiermanager.entity.StockStrategyEntity;
 import io.philoyui.qmier.qmiermanager.service.DataDayService;
@@ -11,17 +10,12 @@ import io.philoyui.qmier.qmiermanager.utils.TalibUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
-/**
- * 10日线，20日线，30日线多头排列
- */
 @Component
-public class MaUpFilter implements StockFilter {
+public class Ma20UpFilter implements StockFilter {
 
     @Autowired
     private DataDayService dataDayService;
@@ -31,12 +25,11 @@ public class MaUpFilter implements StockFilter {
 
     @Override
     public String filterName() {
-        return "ma_up";
+        return "20ma";
     }
 
     @Override
     public Set<String> filterSymbol(StockStrategyEntity stockStrategyEntity) {
-
         SearchFilter searchFilter = SearchFilter.getDefault();
         List<StockEntity> stockEntities = stockService.list(searchFilter);
 
@@ -45,15 +38,11 @@ public class MaUpFilter implements StockFilter {
         for (int i = 0; i < stockEntities.size(); i++) {
             StockEntity stockEntity = stockEntities.get(i);
             double[] closeArray = dataDayService.findCloseData(stockEntity);
-            double[] ma10Array = TalibUtils.ma(closeArray, 10);
             double[] ma20Array = TalibUtils.ma(closeArray, 20);
-            double[] ma30Array = TalibUtils.ma(closeArray, 30);
-
-            if ( ma10Array[0] > ma10Array[1] && ma20Array[0] > ma20Array[1] && ma30Array[0] > ma30Array[1]) {
+            if ( ma20Array[0] > ma20Array[1] && ma20Array[1] < ma20Array[2]) {
                 symbolSet.add(stockEntity.getSymbol());
             }
         }
-
         return symbolSet;
     }
 }
