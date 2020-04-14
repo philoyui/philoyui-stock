d498bc5f16fa7ed0341858cd7bd3563aff2ffe4f
philoyui <yang7551735@gmail.com>
2020-04-14 22:14:13 +0800

    修正定时器

--- philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/entity/TagEntity.java
+++ philoyui-stock-manager/src/main/java/io/philoyui/qmier/qmiermanager/entity/TagEntity.java
@@ -1,12 +1,17 @@
 package io.philoyui.qmier.qmiermanager.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 public class TagEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String tagName;
